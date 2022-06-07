40fc9cff08a3836d16b6edbbfb51741375e2b12f
Ewerton <ewerton.viniciusturco@gmail.com>
2022-06-07 20:19:44 -0300

    Service

--- lib/navigation/route.dart
+++ lib/navigation/route.dart
@@ -1,6 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_web/src/components/toogle_switch.dart';
-import 'package:flutter_web/src/screens/client.dart';
+import 'package:flutter_web/src/screens/service_page.dart';
 import 'package:flutter_web/src/screens/home_page.dart';
 
 class Routes extends StatefulWidget {
@@ -16,7 +16,7 @@ class _RoutesState extends State<Routes> {
     return NavigationView(
         content: NavigationBody(index: index, children: const [
           Home(),
-          Client(),
+          Service(),
         ]),
         appBar: const NavigationAppBar(),
         pane: NavigationPane(
---
+++ lib/src/screens/service_page.dart
@@ -0,0 +1,50 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class Service extends StatefulWidget {
+  const Service({Key? key}) : super(key: key);
+
+  @override
+  State<Service> createState() => _ServiceState();
+}
+
+class _ServiceState extends State<Service> {
+  final List<String> services = [
+    'Marcenaria.',
+    'Jardinagem.',
+    'Hidráulica.',
+    'Mecânica.',
+    'Elétrica.',
+    'Serviços Gerais e muito mais.',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldPage(
+        header: const PageHeader(
+            title: Text(
+          "Serviços em alta na plataforma",
+          style: TextStyle(fontSize: 40),
+        )),
+        content: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(25),
+            child: Column(
+                children: services.map((e) {
+              return ListTile(
+                title: Row(
+                  children: [
+                    const Icon(
+                      FluentIcons.increase_indent_arrow,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      e,
+                      style: const TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              );
+            }).toList()),
+          ),
+        ));
+  }
+}
